00fbd4bb136be9b7b19e5b8af4851bb8ddabe0f0
Maxim Moinat <maxim@thehyve.nl>
2016-10-05 10:04:48 +0200

    Restructured folders and added .bat executable

--- execute_drug_mapping.sh
+++ execute_drug_mapping.sh
@@ -16,8 +16,8 @@ echo
 echo "Loading the auh source data and mappings..."
 psql $DATABASE_NAME -c "DROP SCHEMA IF EXISTS auh CASCADE;" # Remove schema auh and all tables in it
 psql $DATABASE_NAME -c "CREATE SCHEMA IF NOT EXISTS auh;"
-psql $DATABASE_NAME -f loadAUHdata.sql
-psql $DATABASE_NAME -f loadManualMappings.sql
+psql $DATABASE_NAME -f sql_map/loadAUHdata.sql
+psql $DATABASE_NAME -f sql_map/loadManualMappings.sql
 
 echo
 echo "Creating mappings..."
@@ -25,32 +25,32 @@ echo "Creating mappings..."
 psql $DATABASE_NAME -c "DROP SCHEMA IF EXISTS map CASCADE;" # Remove all existing map tables
 psql $DATABASE_NAME -c "CREATE SCHEMA IF NOT EXISTS map;"
 printf "%-30s" "ATC to RxNorm: "
-psql $DATABASE_NAME -f map_atcToRxNorm.sql
+psql $DATABASE_NAME -f sql_map/map_atcToRxNorm.sql
 printf "%-30s" "Varenr to RxNorm: "
-psql $DATABASE_NAME -f map_varenr_to_ingredient.sql
+psql $DATABASE_NAME -f sql_map/map_varenr_to_ingredient.sql
 printf "%-30s" "Single ingredient drugs: "
-psql $DATABASE_NAME -f create_drug_strength_single_ingredient.sql
+psql $DATABASE_NAME -f sql_map/create_drug_strength_single_ingredient.sql
 
 # Mappings to different levels. Order of execution not important
 printf "%-30s" "Varenr to Drug Component: "
-psql $DATABASE_NAME -f map_varenr_to_component.sql
+psql $DATABASE_NAME -f sql_map/map_varenr_to_component.sql
 printf "%-30s" "Varenr to Drug Form: "
-psql $DATABASE_NAME -f map_varenr_to_drug_form.sql
+psql $DATABASE_NAME -f sql_map/map_varenr_to_drug_form.sql
 printf "%-30s" "Varenr to Clinical Drug: "
-psql $DATABASE_NAME -f map_varenr_to_clinical_drug.sql
+psql $DATABASE_NAME -f sql_map/map_varenr_to_clinical_drug.sql
 
 echo
 echo "Combining mappings..."
 # Combine all automated mappings
-psql $DATABASE_NAME -f map_unify_varenr_mappings.sql
+psql $DATABASE_NAME -f sql_map/map_unify_varenr_mappings.sql
 # Override drugs that are manually mapped
-psql $DATABASE_NAME -f combine_manual_and_automated_mapping.sql
+psql $DATABASE_NAME -f sql_map/combine_manual_and_automated_mapping.sql
 
 echo
 echo "Statistics:"
 # Stats
-psql $DATABASE_NAME -f stats_complete_mapping.sql
+psql $DATABASE_NAME -f sql_stats/stats_complete_mapping.sql
 
 # Export
 echo "Export to exports/complete_mapping.csv"
-psql $DATABASE_NAME -f export_complete_mapping.sql
+psql $DATABASE_NAME -f sql_map/export_complete_mapping.sql
